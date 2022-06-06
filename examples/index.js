bffacd09f4dba0088f9f23546ac62d7f7e07382a
kiranbandi <bvenkatkiran@gmail.com>
2022-06-06 14:44:49 -0600

    adding testkit example updates

--- examples/index.js
+++ examples/index.js
@@ -1,261 +1,78 @@
 let gt
 
-async function main () {
-  // get all the elements
-  const connectBtn = document.getElementById('connectbtn')
-  const ipInput = document.getElementById('ip')
-  const idInput = document.getElementById('id')
-  const authBtn = document.getElementById('authbtn')
-  const disconnectedDiv = document.getElementById('disconnected')
-  const connectedDiv = document.getElementById('connected')
-  const authedDiv = document.getElementById('authed')
-  const roomInput = document.getElementById('room')
-  const joinBtn = document.getElementById('joinbtn')
-  const joinedDiv = document.getElementById('joined')
-  const leaveRoomBtn = document.getElementById('leaveroombtn')
-  const info = document.getElementById('info')
-  const users = document.getElementById('users')
-  const state = document.getElementById('state')
-  const updateuserInput = document.getElementById('updateuser')
-  const updateuserBtn = document.getElementById('updateuserbtn')
-  const updateuserBtnUnr = document.getElementById('updateuserbtnunr')
-  const updatestateInput = document.getElementById('updatestate')
-  const updatestateBtn = document.getElementById('updatestatebtn')
-  const updatestateBtnUnr = document.getElementById('updatestatebtnunr')
-  const startingUserStateInput = document.getElementById('user')
+var ourid = 'U-' + Math.floor(Math.random() * 10000);
+
+async function main() {
 
   const createGt = (ip) => {
+
     const gt = new GT(ip, { handleState: true })
-
-    gt.on('users_object_updated', () => {
-      users.value = JSON.stringify(gt.users)
-    })
-
-    gt.on('state_object_updated', () => {
-      state.value = JSON.stringify(gt.state)
-    })
-
-    // fires when WE disconnect.
-    gt.on('disconnect', (reason) => {
-      console.log(`We have disconnected from the server: ${reason}`)
-
-      connectBtn.innerText = 'Connect'
-
-      disconnectedDiv.hidden = false
-      connectedDiv.hidden = true
-      authedDiv.hidden = true
-      joinedDiv.hidden = true
-    })
 
     // fires when we joined a room
     gt.on('joined', (room, roomState, users) => {
-      console.log('Joined', room, 'with state', roomState, 'users', users)
-    })
 
-    // fires when we authed with an id.
-    gt.on('authed', (id, state) => {
-      console.log('Authed', id, 'with state', state)
+      // if there are users in the room create a cursor for each of them
+      Object.keys(users).map((key) => createCursor(key, users[key]));
 
-      startingUserStateInput.value = JSON.stringify(state)
-    })
-
-    // fires when WE left the room
-    gt.on('leftroom', (reason) => {
-      console.log(`We have left the room: ${reason}`)
-
-      disconnectedDiv.hidden = true
-      connectedDiv.hidden = true
-      authedDiv.hidden = false
-      joinedDiv.hidden = true
+      var rootElement = $('body');
+      var topOffsetX = rootElement.offset().left;
+      var topOffsetY = rootElement.offset().top;
+      // broadcast mousemovement to all other connected peers
+      rootElement.on('mousemove', (e) => {
+        gt.updateUser({ 'x': e.pageX - topOffsetX, y: e.pageY - topOffsetY });
+      })
     })
 
     // fires when someone has joined the room (including ourselves).
     gt.on('connected', (id, userState) => {
-      console.log(`ID ${id} has joined with state:`, userState)
+      createCursor(id);
     })
 
     // fires when someone left the room
     gt.on('disconnected', (id, reason) => {
-      console.log(`ID ${id} has left with reason: ${reason}`)
+      $('#' + id).remove();
     })
 
     // these will fire when the room/user state changes.
     gt.on('user_updated_reliable', (id, payloadDelta) => {
-      console.log(`ID ${id} has updated their state:`, payloadDelta)
-    })
-    gt.on('user_updated_unreliable', (id, payloadDelta) => {
-      console.log(`ID ${id} has updated their state:`, payloadDelta)
-    })
-
-    gt.on('state_updated_reliable', (id, payloadDelta) => {
-      console.log(`ID ${id} has updated the room's state:`, payloadDelta)
-    })
-    gt.on('state_updated_unreliable', (id, payloadDelta) => {
-      console.log(`ID ${id} has updated the room's state:`, payloadDelta)
+      $('#' + id).css({ 'left': payloadDelta.x + 'px', top: payloadDelta.y + 'px' });
     })
 
     return gt
   }
 
-  // we are not connected. so hide everything but the disconnected div.
-  disconnectedDiv.hidden = false
-  connectedDiv.hidden = true
-  authedDiv.hidden = true
-  joinedDiv.hidden = true
-
-  // setup the buttons.
-  connectBtn.innerText = 'Connect'
-  connectBtn.addEventListener('click', async () => {
-    // if we are not connected...
-    if (connectBtn.innerText === 'Connect') {
-      const connIp = ipInput.value
-      console.log('Connecting to', connIp)
-
-      gt = createGt(connIp)
-
-      connectBtn.innerText = 'connecting...'
-      connectBtn.disabled = true
-
-      // connect to the server
-      try {
-        await gt.connect()
-
-        // we connected! lets go into the connected state.
-        connectBtn.innerText = 'Disconnect'
-        disconnectedDiv.hidden = true
-        connectedDiv.hidden = false
-        authedDiv.hidden = true
-        joinedDiv.hidden = true
-      } catch (e) {
-        // we failed.
-        connectBtn.innerText = 'Connect'
-        console.error(e)
-      } finally {
-        connectBtn.disabled = false
-      }
-
-      return
-    }
-    // we are already connected, lets disconnect.
-
-    console.log('Disconnecting...')
-
-    connectBtn.innerText = 'disconnecting...'
-    connectBtn.disabled = true
-
-    try {
-      await gt.disconnect()
-      // we disconnected.
-      connectBtn.innerText = 'Connect'
-
-      disconnectedDiv.hidden = false
-      connectedDiv.hidden = true
-      authedDiv.hidden = true
-      joinedDiv.hidden = true
-    } catch (e) {
-      // we failed...
-      connectBtn.innerText = 'Disconnect'
-
-      console.error(e)
-    } finally {
-      connectBtn.disabled = false
-    }
-  })
-
-  authBtn.addEventListener('click', async () => {
-    let authId = idInput.value
-    if (authId === '') authId = undefined
-    console.log('Authing with id', authId)
-
-    authBtn.disabled = true
-    try {
-      await gt.auth(authId)
-
-      // we authed, lets go into the authed state.
-      disconnectedDiv.hidden = true
-      connectedDiv.hidden = true
-      authedDiv.hidden = false
-      joinedDiv.hidden = true
-    } finally {
-      authBtn.disabled = false
-    }
-  })
-
-  joinBtn.addEventListener('click', async () => {
-    const roomName = roomInput.value
-    const userStateText = startingUserStateInput.value
-    let userState
-
-    try {
-      userState = JSON.parse(userStateText)
-    } catch {
-      userState = undefined
-    }
-
-    console.log('Joining room', roomName, 'with user state', userState)
-
-    joinBtn.disabled = true
-    try {
-      await gt.join(roomName, userState)
-
-      // we joined! lets go into the joined state.
-      disconnectedDiv.hidden = true
-      connectedDiv.hidden = true
-      authedDiv.hidden = true
-      joinedDiv.hidden = false
-
-      info.innerText = `ID: '${gt.id}', room: '${gt.room}'`
-    } finally {
-      joinBtn.disabled = false
-    }
-  })
-
-  leaveRoomBtn.addEventListener('click', async () => {
-    console.log('Leaving room')
-
-    startingUserStateInput.value = JSON.stringify(gt.users[gt.id])
-
-    leaveRoomBtn.disabled = true
-    try {
-      await gt.leaveRoom()
-
-      // we left! lets go into the authed state.
-      disconnectedDiv.hidden = true
-      connectedDiv.hidden = true
-      authedDiv.hidden = false
-      joinedDiv.hidden = true
-    } finally {
-      leaveRoomBtn.disabled = false
-    }
-  })
-
-  updateuserBtn.addEventListener('click', async () => {
-    const payloadDelta = JSON.parse(updateuserInput.value)
-    console.log('Sending user update', payloadDelta)
-
-    gt.updateUser(payloadDelta)
-  })
-
-  updateuserBtnUnr.addEventListener('click', async () => {
-    const payloadDelta = JSON.parse(updateuserInput.value)
-    console.log('Sending user update unreliable', payloadDelta)
-
-    gt.updateUserUnreliable(payloadDelta)
-  })
-
-  updatestateBtn.addEventListener('click', async () => {
-    const payloadDelta = JSON.parse(updatestateInput.value)
-    console.log('Sending state update', payloadDelta)
-
-    gt.updateState(payloadDelta)
-  })
-
-  updatestateBtnUnr.addEventListener('click', async () => {
-    const payloadDelta = JSON.parse(updatestateInput.value)
-    console.log('Sending state update unreliable', payloadDelta)
-
-    gt.updateStateUnreliable(payloadDelta)
-  })
+  try {
+    gt = createGt('localhost:3001')
+    await gt.connect();
+    await gt.auth(ourid);
+    await gt.join('synvisio', {});
+  }
+  catch (e) {
+    console.error(e)
+  }
 }
 
 main()
+
+// function to create a new cursor in a random color on the box to represent to a peer
+function createCursor(mountID) {
+  if (ourid == mountID) return;
+
+  const  colorScale = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"],
+  // Assign a random color between 0 to 9
+  colorHash = colorScale[Math.floor(Math.random()*10)];
+
+  let cursorElement = "<svg fill='" + colorHash + "' height='25' width='25'><g transform='scale(0.075)'><path d='M247.094,297c-2.831,0-5.549-1.125-7.551-3.129l-82.835-82.868l-34.905,60.448c-2.046,3.544-5.936,5.61-10.017,5.31c-4.081-0.296-7.634-2.896-9.148-6.697L0.825,14.632c-1.58-3.965-0.648-8.489,2.369-11.505C6.212,0.108,10.735-0.82,14.7,0.761l255.317,101.86c3.799,1.515,6.397,5.067,6.693,9.148c0.295,4.079-1.766,7.968-5.308,10.015l-60.435,34.929l82.839,82.878c4.168,4.169,4.168,10.926,0.001,15.096l-39.162,39.186C252.643,295.873,249.927,297,247.094,297z M154.422,182.934c2.813,0,5.534,1.112,7.551,3.129l85.121,85.156l24.067-24.082l-85.126-85.162c-2.347-2.348-3.469-5.651-3.036-8.941c0.433-3.291,2.371-6.191,5.244-7.852l53.427-30.879L29.86,29.801l84.462,211.902l30.853-53.433c1.66-2.874,4.563-4.813,7.854-5.246C153.494,182.964,153.959,182.934,154.422,182.934z'></path></g></svg>";
+  let containerDiv = $('<div/>', { id: mountID, 'class': 'cursor-container' }).appendTo('body');
+
+  containerDiv.html(cursorElement)
+    .append('<span style="color:' + colorHash + '">' + mountID + '</span>')
+    .css({
+      'position': 'absolute',
+      'top': '0px',
+      'left': '0px'
+    });
+}
+
+
+
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,36 +4,7 @@ module.exports = [{
   output: {
     library: 'GT',
     libraryTarget: 'umd',
-    filename: 'gt.umd.dev.js'
-  },
-  devtool: 'source-map'
-},
-{
-  name: 'cjs',
-  target: 'node',
-  entry: './src/index.js',
-  output: {
-    libraryTarget: 'commonjs2',
-    filename: 'gt.cjs.dev.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime',
-              '@babel/plugin-transform-modules-commonjs']
-          }
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js']
+    filename: 'gt.dev.js'
   },
   devtool: 'source-map'
 }]
